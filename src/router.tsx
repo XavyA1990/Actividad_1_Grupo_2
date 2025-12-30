227119a0bf54ede08a8e05370a1a3f26fbd5a595
XavyA1990 <contrerasdeveloper@outlook.com>
2025-12-30 09:44:16 -0500

    MAJOR T4: Checkout page

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
-import {BookDetail} from "@/pages/BookDetail";
+import { BookDetail } from "@/pages/BookDetail";
+import { Checkout } from "@/pages/Checkout";
 
 export const GlobalRouter = () => {
   return (
@@ -8,6 +9,7 @@ export const GlobalRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/books/:id" element={<BookDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
